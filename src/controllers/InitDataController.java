009e57b5e60c7587a4de6e4972706dcb80a0b71d
dpinchuk <pinchuk.dap@gmail.com>
2019-01-22 13:27:21 +0200

    added file data

---
+++ src/controllers/InitDataController.java
@@ -0,0 +1,12 @@
+package controllers;
+
+import models.BookingModel;
+import models.FlightModel;
+
+public class InitDataController {
+
+    FlightModel flightModel = new FlightModel();
+    BookingModel bookingModel = new BookingModel();
+
+
+}
